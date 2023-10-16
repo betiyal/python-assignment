85360e30813ef211d1b08ce6751c1098e777de72
betiyal <111344081+betiyal@users.noreply.github.com>
2023-10-16 16:18:52 +0300

    Add files via upload

---
+++ distance  between 2 point.py
@@ -0,0 +1,7 @@
+x1=eval(input("enter the value of the initial horizontal axis point"))
+x2=eval(input("enter the value of the end horizontal axis point"))
+y1=eval(input("enter the value of the initial vertical axis point"))
+y2=eval(input("enter the value of the end vertical axis point"))
+d=(((x2-x1)**2) + ((y2-y1)**2))**(0.5)
+
+print(d)
